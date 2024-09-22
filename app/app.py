c323223b5dbd4a2b9d4281e4d296b6af1616acec
Bheta <65680585+TheBheta@users.noreply.github.com>
2024-09-21 20:34:55 -0400

    rewrote backend

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/login')
+def login(name=None):
+    return redirect('https://5623171022.propelauthtest.com/en/login')
+
+@app.route('/register')
+def register(name=None):
+    return redirect('https://5623171022.propelauthtest.com/en/signup')
+
+@app.route('/dashboard')
+def dashboard(name=None):
+    return render_template('dashboard.html')
+
+@app.route('/')
+def index(name=None):
+    return render_template('index.html')
+
+if __name__ == '__main__':  # pragma: no cover
+    app.run(port=80)
