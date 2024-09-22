aa33599fe89152fbaf4d05ebe059b5685b412f94
Bheta <65680585+TheBheta@users.noreply.github.com>
2024-09-21 21:09:46 -0400

    made base for tile thing

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, redirect, url_for
 
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/login')
 def login(name=None):
@@ -15,6 +15,11 @@ def register(name=None):
 @app.route('/dashboard')
 def dashboard(name=None):
     return render_template('dashboard.html')
+
+@app.route('/herpedex')
+def herpedex(name=None):
+    return render_template('herpedex.html')
+
 
 @app.route('/')
 def index(name=None):
